43eff7a0ef3691fece2d3baaaf4c3b7e75d48db4
hardyxiao <244551740@qq.com>
2018-07-04 10:30:17 +0800

    ...rm, restore

--- new4.js
+++
@@ -1,5 +0,0 @@
-git is a distributed version control system
-git is free software
-git is a distributed version control system?
-Git is free software distributed under the GPL.
-Git tracks changes of files.
